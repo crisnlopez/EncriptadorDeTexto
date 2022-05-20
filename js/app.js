f30893f8680eb4df9a9db344dc1c4deffd54d072
cris <cristianpo@live.com.ar>
2022-05-20 11:32:29 -0300

    Elimina variable con letras a encriptar Refactor de las funciones para la lógica Crea función mostrarMsg y mostrarInicio

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
 const textInput = document.getElementById("text_input")
 const infoAlert = document.getElementById("info_alert")
+
 const btnEncriptar = document.getElementById("btn_encriptar")
 const btnDesencriptar = document.getElementById("btn_desencriptar")
+const btnCopiar = document.getElementById("btn_copiar")
+
+const outputDiv = document.getElementById("output-container")
+const noMsg = document.getElementById("no_msg")
+const msgEncriptado = document.getElementById("msg_encriptado")
+const muniecoImg = document.querySelector("img[src='img/munieco.svg']")
 
 const noPermitido = ['á','é','í','ó','ú','?','/','!','@','#','$','%','^','&','*','(',')','-','_','=','+','[',']','{','}','`','~','|','\\']
-const encriptado = ['a','e','i','o','u']
+const ingresaTexto = "Ingresa el texto que desees encriptar o desencriptar."
 
 // Funciones
 const encriptar = () => {
   let s = textInput.value.toLowerCase()
 
-  for (let c of noPermitido) {
-    if (s.includes(c)) {
-      return
-    }
+  // Check vacio
+  if (s === "") {
+    mostrarInicio()
+    return
   }
+
+  // Check input valido
+  let valido = inputValido(s)
+  if (!valido) {return}
 
   s = s.replaceAll('e', 'enter')
   s = s.replaceAll('i', 'imes')
@@ -22,14 +33,30 @@ const encriptar = () => {
   s = s.replaceAll('o', 'ober')
   s = s.replaceAll('u', 'ufat')
 
-  console.log(s)
+  // Check mismo encriptado
+  if (s === msgEncriptado.innerText) {return}
 
-  //TODO: mostrar data
-  textInput.value = ""
+  // Check si es un msg distinto
+  if (msgEncriptado.innerText !== ingresaTexto && s !== msgEncriptado.innerText) {
+    msgEncriptado.innerText = s
+    return
+  }
+
+  mostrarMsg(s)
 }
 
 const desencriptar = () => {
   let s = textInput.value
+
+  // Check vacio
+  if (s === "") {
+    mostrarInicio()
+    return
+  }
+
+  // Check input valido
+  let valido = inputValido(s)
+  if (!valido) {return}
 
   s = s.replaceAll('enter','e')
   s = s.replaceAll('imes','i')
@@ -37,8 +64,37 @@ const desencriptar = () => {
   s = s.replaceAll('ober','o')
   s = s.replaceAll('ufat','u')
 
-  // TODO: mostrar data
-  console.log(s)
+  mostrarMsg(s)
+}
+
+const inputValido = input => {
+  for (let c of noPermitido) {
+    if (input.includes(c)) {
+      return false
+    }
+
+    return true
+  }
+}
+
+const mostrarMsg = input => {
+  muniecoImg.classList.add("display-none")
+  noMsg.classList.add("display-none")
+  btnCopiar.classList.remove("display-none")
+  outputDiv.classList.add("output-content")
+  msgEncriptado.classList.add("msg-encriptado")
+
+  msgEncriptado.innerText = input
+}
+
+const mostrarInicio = () => {
+  muniecoImg.classList.remove("display-none")
+  noMsg.classList.remove("display-none")
+  outputDiv.classList.remove("output-content")
+  msgEncriptado.classList.remove("msg-encriptado")
+  btnCopiar.classList.add("display-none")
+
+  msgEncriptado.innerText = ingresaTexto
 }
 
 // Eventos
